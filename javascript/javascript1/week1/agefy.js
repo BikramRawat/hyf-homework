7af4c0886814fe4820e98e8900e80b5aa04c25cf
BikramRawat <mail2bikram44@gmail.com>
2021-05-08 00:30:51 +0200

    Agify calculator done

---
+++ javascript/javascript1/week1/agefy.js
@@ -0,0 +1,6 @@
+const yearOfBirth = prompt("Enter year of Birth: ");
+const yearFuture = prompt("Enter the future year when you want your age?");
+const age = (yearFuture - yearOfBirth);
+console.log("Your year of birth is :" + " " + yearOfBirth + " " + "AD");
+console.log("The year you want your age to be is : "+ " " + yearFuture + " " + "AD");
+console.log(`You will be: ${age} years old in: ${yearFuture} AD`);
