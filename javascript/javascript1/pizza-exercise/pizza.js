681577cd94ed808cacd30e399212c7586ad364e0
BikramRawat <mail2bikram44@gmail.com>
2021-05-08 11:52:39 +0200

    pizza-exercise js file

---
+++ javascript/javascript1/pizza-exercise/pizza.js
@@ -0,0 +1,4 @@
+console.log("I love pizza");
+let favPizza= prompt("Enter your favorite pizza : ");
+let price = prompt("Enter the price of the pizza in $ : ")
+console.log(`New pizza order: "${favPizza}". The price of the pizza is : $${price}`);
