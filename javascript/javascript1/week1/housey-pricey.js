1dacbcd587b6258f79ca72acdf45f122cb2d895d
Bikram Rawat <81181911+BikramRawat@users.noreply.github.com>
2021-05-11 22:09:16 +0200

    UPDATED WITH SNAKE CASE AND used const

--- javascript/javascript1/week1/housey-pricey.js
+++ javascript/javascript1/week1/housey-pricey.js
@@ -1,13 +1,13 @@
 //For Peter
-let height = 10;
-let width = 8;
-let depth = 10;
-let gardenSizeInM2 = 100;
-let paidPrice = 2500000;
-let volumeInMeters = height * width *depth;
-let housePrice = volumeInMeters * 2.5 * 1000 + gardenSizeInM2 * 300 ;
+const PETER_HEIGHT = 10;
+const PETER_WIDTH = 8;
+const PETER_DEPTH = 10;
+const gardenSizeInM2 = 100;
+const paidPrice = 2500000;
+const volumeInMeters = PETER_HEIGHT * PETER_WIDTH *PETER_DEPTH;
+const housePrice = volumeInMeters * 2.5 * 1000 + gardenSizeInM2 * 300 ;
 console.log(`The price of Peters house should be ${housePrice} but he paid ${paidPrice} `);
-let priceDiff = paidPrice - housePrice;
+const priceDiff = paidPrice - housePrice;
 if(priceDiff>0){
     console.log("Peter is paying too much");
 }
@@ -15,12 +15,12 @@ if(priceDiff>0){
         console.log("Peter you are lucky");
     }
 // For Julia 
- height = 8;
- width = 5;
- depth = 11;
+ const JULIA_HEIGHT = 8;
+ const JULIA_WIDTH = 5;
+ const JULIA_DEPTH = 11;
  gardenSizeInM2 = 70;
  paidPrice = 1000000;
- volumeInMeters = height * width *depth;
+ volumeInMeters = JULIA_HEIGHT * JULIA_WIDTH *JULIA_DEPTH;
  housePrice = volumeInMeters * 2.5 * 1000 + gardenSizeInM2 * 300 ;
 console.log(`The price of Juliass house should be ${housePrice} but she paid ${paidPrice} `);
 priceDiff = paidPrice - housePrice;
