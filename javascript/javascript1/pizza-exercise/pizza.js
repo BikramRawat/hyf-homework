f1acfb7e01191149e001fae645b480bec00622fe
Bikram Rawat <81181911+BikramRawat@users.noreply.github.com>
2021-05-11 21:12:09 +0200

    Update pizza.js
    replaced let with const

--- javascript/javascript1/pizza-exercise/pizza.js
+++ javascript/javascript1/pizza-exercise/pizza.js
@@ -1,4 +1,4 @@
 console.log("I love pizza");
-let favPizza= prompt("Enter your favorite pizza : ");
-let price = prompt("Enter the price of the pizza in $ : ")
+const favPizza= prompt("Enter your favorite pizza : ");
+const price = prompt("Enter the price of the pizza in $ : ")
 console.log(`New pizza order: "${favPizza}". The price of the pizza is : $${price}`);
