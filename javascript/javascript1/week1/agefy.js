65a2473ec710271eff81b8590f9a656dccc9ba81
Bikram Rawat <81181911+BikramRawat@users.noreply.github.com>
2021-05-11 21:39:06 +0200

    Update agefy.js

--- javascript/javascript1/week1/agefy.js
+++ javascript/javascript1/week1/agefy.js
@@ -1,6 +1,6 @@
 const yearOfBirth = prompt("Enter year of Birth: ");
 const yearFuture = prompt("Enter the future year when you want your age?");
-const age = (yearFuture - yearOfBirth);
+const age = yearFuture - yearOfBirth;
 console.log("Age-ify (A future age calculator):-");
 console.log("Your year of birth is :" + " " + yearOfBirth + " " + "AD");
 console.log("The year you want your age to be is : "+ " " + yearFuture + " " + "AD");
