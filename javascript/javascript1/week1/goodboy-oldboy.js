d066837d854d693f002577fdcea192f727244e4f
Bikram Rawat <81181911+BikramRawat@users.noreply.github.com>
2021-05-11 22:01:40 +0200

    updated with comments

--- javascript/javascript1/week1/goodboy-oldboy.js
+++ javascript/javascript1/week1/goodboy-oldboy.js
@@ -1,11 +1,11 @@
 const dogYearOfBirth = prompt("Enter the birthyear of the dog : ");
 const dogYearFuture = prompt("Enter the future year you want to be for the dog ?");
-let dogYear = dogYearFuture - dogYearOfBirth;
+const dogYear = dogYearFuture - dogYearOfBirth;
 //let shouldShowResultInDogYears = false;
-let shouldShowResultInDogYears = confirm("Shoud result show in dog years ? hint: if yes?click Ok else click calcel:");
+const shouldShowResultInDogYears = confirm("Shoud result show in dog years ?\n hint: if yes?click Ok else click calcel:");
 console.log("Goodboy-Oldboy (A dog age calculator):- ");
-console.log("Dog year of birth is : " + " " + dogYearOfBirth + " " +"AD");
-console.log("Dog year future you want in is : " + " " + dogYearFuture + " " +"AD");
+console.log(`Dog year of birth is : ${dogYearOfBirth} AD`);
+console.log(`Dog year future you want in is :${dogYearFuture} AD`);
 /*if(!shouldShowResultInDogYears){
     console.log(`Your dog will be ${dogYear*7} dog years old in ${dogYearFuture} AD`);
 }
