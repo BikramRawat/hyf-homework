584643de298011a7df46fd8566d5e53ed1d54ba5
Bikram Rawat <81181911+BikramRawat@users.noreply.github.com>
2021-05-20 21:26:36 +0200

    Update student_manager.js

--- javascript/javascript1/week2/student_manager.js
+++ javascript/javascript1/week2/student_manager.js
@@ -1,5 +1,5 @@
-const class07Students = [];
-function addStudentToClass(studentName) {
+  const class07Students = [];
+  function addStudentToClass(studentName) {
   // You write code here
   if(class07Students.length<6 || studentName ==='Queen'){
       class07Students.push(studentName);
@@ -14,23 +14,23 @@ function addStudentToClass(studentName) {
   }
 }
 
-function getNumberOfStudents() {
+ function getNumberOfStudents() {
   // You write code here
   console.log(`Class 07 have ${class07Students.length} students !`);
 }
-addStudentToClass('Benjamin');
-addStudentToClass('Riya');
-addStudentToClass('Aarohi');
-addStudentToClass('Aarohi');
-//when i repeat the same name , it should not push it to the array
-//but its accepting, i tried to resolve
-//but couldnot
-addStudentToClass('Tudor');
-addStudentToClass('Arsha');
-addStudentToClass('Anca');
-addStudentToClass('Olivia');
-addStudentToClass('Austin');
-addStudentToClass('Queen');
-addStudentToClass('Queen');
-getNumberOfStudents();
-console.log(`The list of students name is : ${class07Students}`);
+  addStudentToClass('Benjamin');
+  addStudentToClass('Riya');
+  addStudentToClass('Aarohi');
+  addStudentToClass('Aarohi');
+  //when i repeat the same name , it should not push it to the array
+  //but its accepting, i tried to resolve
+  //but couldnot
+  addStudentToClass('Tudor');
+  addStudentToClass('Arsha');
+  addStudentToClass('Anca');
+  addStudentToClass('Olivia');
+  addStudentToClass('Austin');
+  addStudentToClass('Queen');
+  addStudentToClass('Queen');
+  getNumberOfStudents();
+  console.log(`The list of students name is : ${class07Students}`);
