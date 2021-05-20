cc10d604084ceb19e5fe05188816420203485fd5
Bikram Rawat <81181911+BikramRawat@users.noreply.github.com>
2021-05-20 11:09:57 +0200

    updated with feedbacks

--- javascript/javascript1/week2/weather_wear.js
+++ javascript/javascript1/week2/weather_wear.js
@@ -1,21 +1,27 @@
 function weatherBasedWear(temp){
     if(temp<=0){
         return `wear a long warm thermo/winter coats: its very cold`;
-    }else if(temp<=20){
+    }
+    else if(temp<=20){
         return `you can wear normal dress with some jacket or sweater`;
-    }else if(temp<=35){
+    }
+    else if(temp<=35){
         return `Wear half shorts and t-shirts`;
-    } else{
+    }
+    else{
         return `Its too hot , dont go outside or use proper sun glas and light white clothes`;
     }
 }
 const clothesTOWear1=weatherBasedWear(15);
-const clothesTOWear2=weatherBasedWear(40);
-const clothesTOWear3=weatherBasedWear(-7);
-const clothesTOWear4=weatherBasedWear(30);
 console.log(clothesTOWear1);
+
+const clothesTOWear2=weatherBasedWear(40);
 console.log(clothesTOWear2);
+
+const clothesTOWear3=weatherBasedWear(-7);
 console.log(clothesTOWear3);
+
+const clothesTOWear4=weatherBasedWear(30);
 console.log(clothesTOWear4);
 
 
