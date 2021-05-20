90ca9a090f621d0c3d5fe6034e7c465b882c0b1f
Bikram Rawat <81181911+BikramRawat@users.noreply.github.com>
2021-05-20 10:52:16 +0200

    feednack fixed

--- javascript/javascript1/week2/event_application.js
+++ javascript/javascript1/week2/event_application.js
@@ -1,10 +1,14 @@
-function getEventWeekday(futureEventdayNumber){
-    const today= new Date();
+
+    
     const daysOfWeek=['MOnday','Tuesday','Wednsday','Thursday','Friday','Saturday','Sunday'];
+    function getEventWeekday(futureEventdayNumber){
+    const today= new Date();
     const dayOfEvent=((today.getDay() + futureEventdayNumber)%7)-1; //since array index starts from 0 
-    console.log(`Today is : ${today}\n \nThe number for the day is: ${today.getDay()}`);
+        return daysOfWeek[daoOfEvent];
+   
+        /*console.log(`Today is : ${today}\n \nThe number for the day is: ${today.getDay()}`);
     console.log(`The number for the event day in future is: ${futureEventdayNumber}`);
-    console.log(`Therefore:the event will be held on : ${daysOfWeek[dayOfEvent]} `);
+    console.log(`Therefore:the event will be held on : ${daysOfWeek[dayOfEvent]} `);*/
 }
 getEventWeekday(13);
 getEventWeekday(7);
