7d079a77818093a34507174447f62afa3b60d858
BikramRawat <mail2bikram44@gmail.com>
2021-05-21 23:07:03 +0200

    updated with feedbacks

--- javascript/javascript1/week3/removeName.js
+++ javascript/javascript1/week3/removeName.js
@@ -7,13 +7,7 @@ const names = [
     "Samuel",
     "katrine",
     "Tala",
-  ];
-  const nameToRemove = "Ahmad";
-  //names.splice(1,1);
-  names.splice(names.indexOf(nameToRemove),1);
-  
-  // Write some code here
-  
-  // Code done
-  
-  console.log(names); // ['Peter', 'Yana', 'kristina', 'Rasmus', 'Samuel', 'katrine', 'Tala']
+];
+const nameToRemove = "Ahmad";
+names.splice(names.indexOf(nameToRemove), 1);
+console.log(names);
