fd4198ac9a664229727b616db596624f3d597ce0
BikramRawat <mail2bikram44@gmail.com>
2021-05-21 23:05:41 +0200

    updated with feedbacks

--- javascript/javascript1/week3/noteApp.js
+++ javascript/javascript1/week3/noteApp.js
@@ -1,26 +1,19 @@
 const notes = [];
-
 function saveNote(content, id) {
-  // write some code here
-  let noteInfo = {
-      content:content,
-      id:id
-  }
-  notes.push(noteInfo);
+    const noteInfo = {
+        content,id
+    }
+    notes.push(noteInfo);
 }
-
 saveNote('Pick up groceries', 1);
 saveNote('Do laundry', 2);
-saveNote('Doctors appointment',7);
-saveNote('call mom',5);
-saveNote('watch football',2);//another note with same id with anothe content
-
+saveNote('Doctors appointment', 7);
+saveNote('call mom', 5);
+saveNote('watch football', 2);//another note with same id with another content
 console.log(notes);
-// [{content: 'Pick up groceries', id: 1}, {content: 'Do laundry', id: 2}]
-function getNote(id){
-    //your code here
-    for(let i=0;i<notes.length;i++){
-        if(notes[i].id === id){
+function getNote(id) {
+    for (let i = 0; i < notes.length; i++) {
+        if (notes[i].id === id) {
             return notes[i];
         }
     }
@@ -37,14 +30,10 @@ console.log(anotherNote);
 const nextNote = getNote(5);
 console.log(nextNote);
 
-function logOutNotesFormatted(){
-    //your code here
-    for(let i=0;i<notes.length;i++){
+function logOutNotesFormatted() {
+    for (let i = 0; i < notes.length; i++) {
         console.log(`The note with id: ${notes[i].id}, has the following text:
         "${notes[i].content} !"`);
     }
 }
-logOutNotesFormatted(); 
-//should log out the text below
-// The note with id: 1, has the following note text: Pick up groceries
-// The note with id: 2, has the following note text: Do laundry
+logOutNotesFormatted();
