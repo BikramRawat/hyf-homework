081317a9f7930a02b2769969a26900902a668030
BikramRawat <mail2bikram44@gmail.com>
2021-05-21 10:01:21 +0200

    first.js renamed to removeName.js

---
+++ javascript/javascript1/week3/removeName.js
@@ -0,0 +1,19 @@
+const names = [
+    "Peter",
+    "Ahmad",
+    "Yana",
+    "kristina",
+    "Rasmus",
+    "Samuel",
+    "katrine",
+    "Tala",
+  ];
+  const nameToRemove = "Ahmad";
+  //names.splice(1,1);
+  names.splice(names.indexOf(nameToRemove),1);
+  
+  // Write some code here
+  
+  // Code done
+  
+  console.log(names); // ['Peter', 'Yana', 'kristina', 'Rasmus', 'Samuel', 'katrine', 'Tala']
