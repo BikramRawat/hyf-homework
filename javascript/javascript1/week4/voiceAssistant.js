fdef112b46d1a6a4556dabaa27341ef8f4ccef3e
BikramRawat <mail2bikram44@gmail.com>
2021-06-09 21:49:47 +0200

    voice assistant file updated with bugs fixed

--- javascript/javascript1/week4/voiceAssistant.js
+++ javascript/javascript1/week4/voiceAssistant.js
@@ -1,119 +1,116 @@
 let myName = "";
 const toDoList = [];
 
-function getReply(command) 
-{
-    let x = command.toLowerCase();
-    const start = x.indexOf("is");
-    const name = x.substring(start + 2).trim();
-if (x.includes("my name is")) 
-{
-    if (name == "") 
-    {
-        console.log('Please enter your name')
-        myName = prompt('What is your name?')
-        return `Nice to meet you ${myName}`;
-    } 
-    else {
-    myName = name
-    return `Nice to meet you ${myName}`;
-    }
-} 
-else if (x.includes("what is my name")) 
-{
-    return `Your name is ${myName}`;
-} 
-else if (x.includes("to my todo")) 
-{
-    const start = x.indexOf("add");
-    const end = x.indexOf("to my todo");
-    const toDoItem = x.substring(start + 4, end - 1);
-    toDoList.push(toDoItem);
-    return `${toDoItem} added to your todo`;
+function getReply(command) {
 
-} 
-else if (x.includes("from my todo"))
-{
-    const begin = x.indexOf("remove")
-    const last = x.indexOf("from my todo");
-    const toDoItem = x.substring(begin + 7, last - 1);
-    const index = toDoList.indexOf(toDoItem);
-    if (index !== -1) 
-    {
-        toDoList.splice(index, 1);
-        return `${toDoItem} removed from your todo`;
+    command = command.toLowerCase();
+    
+    if (command.includes("my name is")){
+        const start = command.indexOf("is");
+        const name = command.substring(start + 2).trim();
+        if (!name){
+            console.log('Please enter your name')
+            myName = prompt('What is your name?')
+            return `Nice to meet you ${myName}`;
+        } 
+
+        else{
+            myName = name
+            return `Nice to meet you ${myName}`;
+        }
     } 
-    else
-    {
-        return `${toDoItem} is not in your todo`;
-    }
-} 
-else if (x.includes("what is on my todo"))
-{
-    let listOfItem = "";
-    for (let i = 0; i < toDoList.length; i++) 
-    {
-        if (i === 0) {
+    else if (command.includes("what is my name")){
+        return `Your name is ${myName}`;
+    } 
+    else if (command.includes("to my todo")){
+        const start = command.indexOf("add");
+        const end = command.indexOf("to my todo");
+        const toDoItem = command.substring(start + 4, end - 1);
+        toDoList.push(toDoItem);
+        return `${toDoItem} added to your todo`;
+    } 
+    else if (command.includes("from my todo")){
+        const begin = command.indexOf("remove")
+        const last = command.indexOf("from my todo");
+        const toDoItem = command.substring(begin + 7, last - 1);
+        const index = toDoList.indexOf(toDoItem);
+        if (index !== -1){
+            toDoList.splice(index, 1);
+            return `${toDoItem} removed from your todo`;
+        } 
+        else{
+            return `${toDoItem} is not in your todo`;
+        }
+    } 
+    else if (command.includes("what is on my todo")){
+        let listOfItem = "";
+        for (let i = 0; i < toDoList.length; i++){
+            if (i === 0){
             listOfItem = toDoList[i];
-        } 
-        else if (i === toDoList.length - 1)
-        {
+            } 
+            else if (i === toDoList.length - 1){
             listOfItem = `${listOfItem}  and ${toDoList[i]}`;
-        } 
-        else 
-        {
+            } 
+            else{
             listOfItem = `${listOfItem} , ${toDoList[i]}`;
         }
     }
     return `you have ${toDoList.length} todo's - ${listOfItem}`;
-} 
-else if (x.includes("what day is it today"))
-{
-    const today = new Date();
-    const dateArr = today.toString().split(" ");
-    return `${dateArr[2]}. of ${dateArr[1]} ${dateArr[3]}`;
-}
-else if (x.includes("what is")) 
-{
-    const arr = x.split(" ");
-    const num1 = parseInt(arr[2]);
-    const num2 = parseInt(arr[4]);
-    if (arr[3] === "+") 
-    {
-        return num1 + num2;
     } 
-    else if (arr[3] === "*") 
-    {
-        return num1 * num2;
-    } 
-    else if (arr[3] === "/") 
-    {
-        return num1 / num2;
-    } 
-    else if (arr[3] === "-") 
-    {
-        return num1 - num2;
+    else if (command.includes("what day is it today")){
+        const today = new Date();
+        const dateArr = today.toString().split(" ");
+        return `${dateArr[2]}. of ${dateArr[1]} ${dateArr[3]}`;
     }
-}
-else if (x.includes("set a timer for ")) 
-{
-    const arr = x.split(" ");
-    let timer = 0;
-    if (arr[arr.length - 1].includes("sec")) 
-    {
-        timer = arr[arr.length - 2] * 1000;
-    } 
-    else if (arr[arr.length - 1].includes("min")) {
-        timer = arr[arr.length - 2] * 60000;
-    } 
-    else if (arr[arr.length - 1].includes("hour")) {
-        timer = arr[arr.length - 2] * 360000;
+    else if (command.includes("what is")){
+        //const array = command.split(" "); 
+        const [ , , number1, operator, number2] = command.split(' ');
+        if (operator === '+'){
+            return number1 + number2;
+        }
+        else if (operator === "*"){
+            return number1 * number2;
+        } 
+        else if (operator === "/"){
+            return number1 / number2;
+        } 
+        else if (operator === "-"){
+            return number1 - number2;
+        }
+        /*
+        const num1 = parseInt(array[2]);
+        const num2 = parseInt(array[4]);
+        if (array[3] === "+"){
+            return num1 + num2;
+        } 
+        else if (array[3] === "*"){
+            return num1 * num2;
+        } 
+        else if (array[3] === "/"){
+            return num1 / num2;
+        } 
+        else if (array[3] === "-"){
+            return num1 - num2;
+        }
+        */
     }
+    else if (command.includes("set a timer for ")){
+        const array = command.split(" ");   
+        let timer = 0;
+        if (array[array.length - 1].includes("sec")){
+            timer = array[array.length - 2] * 1000;
+        }    
+        else if (array[array.length - 1].includes("min")){
+            timer = array[array.length - 2] * 60000;
+        } 
+        else if (array[array.length - 1].includes("hour")){
+            timer = array[array.length - 2] * 360000;
+        }
     setTimeout(function () {
         alert("Timer OFF!")
     }, timer);
-    return `Timer set for ${arr[arr.length-2]} ${arr[arr.length-1]}`;
-}
+    return `Timer set for ${array[array.length-2]} ${array[array.length-1]}`;
+    }
 }
 
 console.log(getReply("Hello my name is Benjamin"));
@@ -126,7 +123,7 @@ console.log(getReply("Add doctors appointment to my todo"));
 console.log(getReply("Remove fishing from my todo"));
 console.log(getReply("What is on my todo?"));
 console.log(getReply("What day is it today?"));
-console.log(getReply("what is 3 + 3"));
+console.log(getReply("what is 4 + 3 "));
 console.log(getReply("what is 3 * 3"));
 console.log(getReply("what is 12 / 4"));
 console.log(getReply("what is 23 - 12"));
