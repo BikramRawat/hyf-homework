f5362044527f748f6ba1e59e1a12c5ccdbe688c1
BikramRawat <mail2bikram44@gmail.com>
2021-06-29 23:05:23 +0200

    different exercises added in one folder

---
+++ javascript/javascript3/week2/homeworksAll/getCurrentPosition.js
@@ -0,0 +1,22 @@
+// Create navigator.geolocation.getCurrentPosition promise 
+//and write the code to use that promise
+function getCurrentLocation(time){
+    return new Promise((resolve,reject)=> {
+        setTimeout(() => {
+            navigator.geolocation.getCurrentPosition((position) => {
+                try {
+                    resolve(position);
+                } catch (error) {
+                    reject(error);
+                }
+            })
+        }, time);
+    })
+}
+getCurrentLocation(2000)
+.then((position) => {
+    console.log('Users Position found : ', position);
+})
+.catch((error)=>{
+    console.warn('Error getting users location', error);
+});
