fe9c324c49e393f36919f43a9a5d7bfdc4fc53d6
Bikram Rawat <mail2bikram44@gmail.com>
2021-09-07 10:26:49 +0200

    extra component getTodos added

---
+++ react/week1/todo-list-app/src/getTodos.js
@@ -0,0 +1,19 @@
+// Array of todo's Component
+export default function getTodos() {
+  const todoArray = [
+    {
+      todoDescription: "Get out of bed",
+      deadline: "Wed Sep 13 2021",
+    },
+    {
+      todoDescription: "Brush teeth",
+      deadline: "Thu Sep 14 2021",
+    },
+    {
+      todoDescription: "Eat breakfast",
+      deadline: "Fri Sep 15 2021",
+    },
+  ];
+  return todoArray;
+}
+// export default getTodos;
